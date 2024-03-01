261cc5acfd95b7fcc63d92c3faf4954e30010afb
ashifshaikhtechbrainscorp <ashif.shaikh@techbrainscorp.com>
2024-03-01 15:37:48 +0530

    Addition of JSON and block to give keywords and strong KW in the code.

--- common_words.py
+++ common_words.py
@@ -1,4 +1,4 @@
 def excludedWords():
-    return ('image', 'style', 'give', 'side', 'may', 'someone', 'told', 'said',
+    return ('image', 'style', 'give', 'side','mm','hg','cdc', 'may', 'someone', 'told', 'said',
         'doctors', 'top', 'people', 'cells', 'baby','name', 'city','also', 'us','read','go','j','ratemds',
         'day','make','know','many','like','help','holidays','well','holiday','advertisement','ratemds','doctorfind','epoch')
